6893f577111701fdd3d4ea15b618b113f7e119a8
Victor <victor.velasco01@hotmail.com>
2021-01-30 00:02:38 -0600

    Something like my first "Hello world" but in Web Programming

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hola mundo</title>
+</head>
+
+<body>
+  <?php
+      print"Bienvenidos a mi primer pagina web<br>";
+      print "Hola mundo";
+
+   ?>
+</body>
+</html>
