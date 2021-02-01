1e5d8a0c929e53c5f4a5df80724f1f04e89360df
Victor <victor.velasco01@hotmail.com>
2021-02-01 17:15:10 -0600

    My first function in PHP!

--- segunda_pagina.php
+++ segunda_pagina.php
@@ -5,11 +5,18 @@
 <title>Segunda pagina</title>
 <body>
   <?php
-  $nombre="Victor Velasco";
-  $edad=18;
-  print "El nombre del usuario es: $nombre<br>";
-  echo $nombre,$edad;
+    $nombre="Victor Velasco";
+    $edad=18;
+    print "El nombre del usuario es: $nombre<br>";
+    echo $nombre,$edad;
+    echo "<br>Este es el primer mensaje<br>";
+    echo "Este es el segundo mensaje<br>";
 
+    function dameDatos(){
+      echo "Este es el mensaje del interior de la funcion!<br>";
+
+    }
+    dameDatos();
   ?>
 </body>
 </html>
